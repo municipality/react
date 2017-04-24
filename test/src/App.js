00a0cb43f682d94e61bf413a51047009b91c6022
Brian <amunicipality@gmail.com>
2017-04-23 17:53:48 -0700

    Added scss usage

--- test/src/App.js
+++ test/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './navbar';
+import Navbar from './components/navbar';
 
 class App extends Component {
   render() {
--- test/src/index.js
+++ test/src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App';
-import Navbar from './navbar';
-import './index.css';
+import App from './App';
+import './app.css';
 
 ReactDOM.render(
-  <Navbar />,
+  <App />,
   document.getElementById('root')
 );
